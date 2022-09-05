d2ab8312c718b434ef7a1562b88f31088fb73c34
valestiny <k.42valeriya@gmail.com>
2022-09-05 22:25:32 +0700

    Create Zadanie.4.js

---
+++ Zadanie.4.js
@@ -0,0 +1,13 @@
+function printNumbers(from, to) {
+  let current = from;
+  function go() {
+    console.log(current);
+    if (current === to) {
+      clearInterval(timerId);
+    }
+    current++;
+  }
+  go();
+  let timerId = setInterval(go, 1000);
+}
+printNumbers(5, 15);
